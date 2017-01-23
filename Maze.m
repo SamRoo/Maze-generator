096edc82be9c4e74e92d0b5de9cc051d730bc4bb
Sam Roo <s.roo@warwick.ac.uk>
2017-01-23 02:58:35 +0000

    Fully working code. Need to remove try-catches.

--- Maze.m
+++ Maze.m
@@ -1,60 +1,56 @@
-dim = input('Enter a maze size ');
-% Creates the grid and sets all of the maze to walls
-maze = ones(dim,dim); 
+%{
+Maze generator
+Creates the maze by creating a random spanning tree using a randomised 
+version of Prim's algorithm 
+Animation of generation causes a significant slowdown
+%}
 
-% Selects random starting point  
-row = 2;  
-col = 2;
-maze(row,col) = 0;
-
-% Creates the frontier 
-global frontier; 
-updateFrontier(maze, row, col); 
-
-% While there is still maze frontier
-while size(frontier,2) > 0 
-
-    % Choose an unvisited element from the frontier
-    r = randi(size(frontier,2));
-    row = frontier(1,r);
-    col = frontier(2,r);
-
-    % If the frontier element has been visited remove it and repick
-    if (maze(row,col) == 0) 
-        frontier(:,r) = []; %the rth column
-        continue;
-    end
-
-    % Create list of visited cells that the frontier f could connect to
-    visited = [];
-    % tries used in case new frontier cell is out of bounds
-    try if (maze(row-2, col  ) == 0); visited = [visited, [row-2; col  ]];end;catch;end;
-    try if (maze(row+2, col  ) == 0); visited = [visited, [row+2; col  ]];end;catch;end;
-    try if (maze(row, col - 2) == 0); visited = [visited, [row; col - 2]];end;catch;end;
-    try if (maze(row, col + 2) == 0); visited = [visited, [row; col + 2]];end;catch;end;
-
-    % Select random passage      
-    r = randi(size(visited,2));
-    vrow = visited(1,r);
-    vcol = visited(2,r);
-
-    % Adds two new passages and updates figure twice       
-    maze((row+vrow)/2, (col+vcol)/2) = 0;
-    imshow(maze,'InitialMagnification','fit');
-    drawnow;
-    maze(row,col) = 0;
-    drawnow;
-
-    % Updates the frontier
-    frontier(:,r) = [];
-    updateFrontier(maze, row, col); 
+dim = 0;
+while dim < 3
+    dim = input('Enter a maze size (must be greater than 2) ');
 end
-clear
+maze = zeros(dim,dim);  % the maze walls are 0 values, passages are 1
+maze(2,1)     = 1;
+row = 2;  col = 2;      % start point
+maze(row,col) = 1;
+f = [];                 % creates the frontier 
+f = updateFrontier(maze, f, row, col); 
 
-function f = updateFrontier(maze, row, col)
-    % tries used in case new frontier cell is out of bounds
-    try if (maze(row-2, col  ) == 1); f = [f, [row-2; col  ]];end;catch;end;
-    try if (maze(row+2, col  ) == 1); f = [f, [row+2; col  ]];end;catch;end;
-    try if (maze(row, col - 2) == 1); f = [f, [row; col - 2]];end;catch;end;
-    try if (maze(row, col + 2) == 1); f = [f, [row; col + 2]];end;catch;end;
+while size(f,2) > 0     % while there is still maze frontier
+    r = randi(size(f,2));   % choose an unvisited element from the frontier
+    row = f(1,r);
+    col = f(2,r);
+    if (maze(row,col) == 1) % if cell has been visited (duplicate)
+        f(:,r) = [];            % removes the it from frontier
+        continue;               % skips iteration to choose another cell
+    end
+    passages = [];          % create list of cells that the frontier could connect to
+    try if (maze(row-2, col  ) == 1); passages = [passages, [row-2; col  ]];end;catch;end
+    try if (maze(row+2, col  ) == 1); passages = [passages, [row+2; col  ]];end;catch;end
+    try if (maze(row, col - 2) == 1); passages = [passages, [row; col - 2]];end;catch;end
+    try if (maze(row, col + 2) == 1); passages = [passages, [row; col + 2]];end;catch;end
+
+    randPass = randi(size(passages, 2));     % select random passage      
+    vrow = passages(1, randPass);
+    vcol = passages(2, randPass);
+
+    maze((row+vrow)/2, (col+vcol)/2) = 1;    % creates intermediate passage 
+    imshow(maze,'InitialMagnification','fit');   % comment out for speed up
+    maze(row,col) = 1;      % creates passage
+    drawnow;                % comment out for speed up
+
+    f(:,r) = [];            % remove the added cell from frontier
+    f = updateFrontier(maze, f, row, col); 
+end
+
+maze(dim-1,dim) = 1;
+imshow(maze,'InitialMagnification','fit'); 
+drawnow;
+
+function f = updateFrontier(maze, frontier, row, col)
+    f = frontier;
+    try if (maze(row-2, col  ) == 0); f = [f, [row-2; col  ]];end;catch;end
+    try if (maze(row+2, col  ) == 0); f = [f, [row+2; col  ]];end;catch;end
+    try if (maze(row, col - 2) == 0); f = [f, [row; col - 2]];end;catch;end
+    try if (maze(row, col + 2) == 0); f = [f, [row; col + 2]];end;catch;end
 end
